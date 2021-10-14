902ed32ef512c7aeadaffef1191c06d4ce2bfe0e
sory <Sorybagayoko5@gmail.com>
2021-10-14 15:39:17 +0200

    add template single post

--- blog-codingfactory/src/components/Single-post.vue
+++ blog-codingfactory/src/components/Single-post.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
+<<<<<<< Updated upstream
     
+=======
+    <div class="grille" style="width: 18rem;">
+    <img :src = "image" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h6 class="card-title">{{ auteur }}</h6>
+              <h5 class="card-subtitle mb-2 text-muted">{{ titre }}</h5>
+            <p class="card-text">{{ description }}</p>
+            <p class ="card-date"> {{ date }} </p>
+        <a :href = "link" class="btn btn-primary">Voir</a>
+        </div>
+     </div>
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+<<<<<<< Updated upstream
 import axios from 'axios'
 import Vue from 'vue'
 import VueAxios from 'vue-axios'
@@ -28,5 +42,56 @@ export default {
       })
     }
   }
-}
+=======
 </script>
+
+<style>
+
+.grille{
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  grid-gap: 10px;
+  margin-top: 30px;
+  border: solid;
+>>>>>>> Stashed changes
+}
+h1{
+  text-align: center;
+  font-size: larger;
+    
+}
+
+.btn-primary {
+    color: #fff;
+    background-color: #0d6efd;
+    border-color: #0d6efd;
+}
+
+.btn-primary {
+    border: 0;
+    line-height: 2.5;
+    padding: 0 20px;
+    font-size: 1rem;
+    text-align: center;
+    color: #fff;
+    text-shadow: 1px 1px 1px #000;
+    border-radius: 10px;
+    background-color: #42b983;
+    background-image: linear-gradient(to top left,
+                                      rgba(0, 0, 0, .2),
+                                      rgba(0, 0, 0, .2) 30%,
+                                      rgba(0, 0, 0, 0));
+    box-shadow: inset 2px 2px 3px rgba(255, 255, 255, .6),
+                inset -2px -2px 3px rgba(0, 0, 0, .6);
+}
+
+.btn-primary:hover {
+    background-color: #42b983;
+}
+
+.btn-primary:active {
+    box-shadow: inset -2px -2px 3px rgba(255, 255, 255, .6),
+                inset 2px 2px 3px rgba(0, 0, 0, .6);
+}
+
+</style>
--- blog-codingfactory/src/views/About.vue
+++ blog-codingfactory/src/views/About.vue
@@ -1,13 +1,22 @@
 <template>
   <div classe="about">
+
     <div classe="title">
       <h1>About</h1>
     </div>
+
     <div class="text-about">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
          Animi atque fugit eius eaque molestiae unde quia fuga, nulla,
-        sed illum ipsum distinctio quam nisi blanditiis beatae earum, magnam debitis libero.</p>
+        sed illum ipsum distinctio quam nisi blanditiis beatae earum, magnam debitis libero.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae,
+        ducimus repudiandae tempore sapiente voluptas magnam perspiciatis dignissimos vitae 
+        mollitia unde inventore maiores ipsam numquam ut aliquid cumque nulla quas sunt!
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas voluptates consectetur sit,
+         ducimus dolore cum perspiciatis? Aperiam nemo optio tempore nisi aliquam beatae ipsam. 
+         Culpa similique blanditiis nisi aliquid temporibus.</p>
     </div>
+
   </div>
 </template>
  
@@ -25,18 +34,24 @@
   color: #22303f;
 }
 
-.title{
-  font: larger;
-  text-align: center;
+h1 {
+    display: block;
+    font-size: 4em;
+    margin-block-start: 0.67em;
+    margin-block-end: 0.67em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
 }
 
 .text-about{
-
-
+  text-align: center;
+  color: #22303f;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin-left: 15%;
+  margin-right: 15%;
+  margin-top: 8%
 }
-
-
-
 
 </style>
 
