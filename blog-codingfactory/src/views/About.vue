a26b6855f731893c870182ea891a9d2c849a3356
sory <Sorybagayoko5@gmail.com>
2021-10-14 11:38:14 +0200

    push about title and lorem

--- blog-codingfactory/src/views/About.vue
+++ blog-codingfactory/src/views/About.vue
@@ -1,5 +1,42 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div classe="about">
+    <div classe="title">
+      <h1>About</h1>
+    </div>
+    <div class="text-about">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Animi atque fugit eius eaque molestiae unde quia fuga, nulla,
+        sed illum ipsum distinctio quam nisi blanditiis beatae earum, magnam debitis libero.</p>
+    </div>
   </div>
 </template>
+ 
+<script>
+
+
+</script>
+<style>
+
+.about{
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #22303f;
+}
+
+.title{
+  font: larger;
+  text-align: center;
+}
+
+.text-about{
+
+
+}
+
+
+
+
+</style>
+
